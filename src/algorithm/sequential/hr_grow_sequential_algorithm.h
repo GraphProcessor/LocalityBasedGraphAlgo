a59429e5d93bdfdff9a96e4c19a53b78322b6fe5
cheyulin <yche@cse.ust.hk>
2017-01-05 22:33:12 +0800

    update hr grow

--- src/algorithm/sequential/hr_grow_sequential_algorithm.h
+++ src/algorithm/sequential/hr_grow_sequential_algorithm.h
@@ -28,52 +28,48 @@
 
 using namespace std;
 
-struct sparsevec {
-    unordered_map<mwIndex, double> map;
+struct sparse_vec {
+    unordered_map<mwIndex, double> weight_map_;
 
     double get(mwIndex index, double default_value = 0.0) {
-        auto it = map.find(index);
-        return it == map.end() ? default_value : it->second;
+        auto it = weight_map_.find(index);
+        return it == weight_map_.end() ? default_value : it->second;
     }
 
     double sum() {
-        return accumulate(map.begin(), map.end(), 0,
+        return accumulate(weight_map_.begin(), weight_map_.end(), 0,
                           [](auto &&left, auto &&right) { return left.second + right.second; });
     }
 
     mwIndex max_index() {
-        return map.size() == 0 ? 0 :
-               max_element(map.begin(), map.end(),
+        return weight_map_.size() == 0 ? 0 :
+               max_element(weight_map_.begin(), weight_map_.end(),
                            [](auto &&left, auto &&right) { return left.second < right.second; })->first;
     }
 };
 
-struct sparserow {
-    mwSize n, m;
-    mwIndex *ai;
-    mwIndex *aj;
-    double *a;
+struct sparse_row {
+    mwSize n_, m_;
+    mwIndex *vertices_;
+    mwIndex *edges_;
+    double *weight_;
 
     mwIndex sr_degree(mwIndex u) {
-        return ai[u + 1] - ai[u];
+        return vertices_[u + 1] - vertices_[u];
     }
 };
 
-//mwIndex sr_degree(sparserow *s, mwIndex u) {
-//    return s->ai[u + 1] - s->ai[u];
-//}
-
-unsigned int get_taylor_degree(const double t, const double eps) {
-    double eps_exp_t = eps * exp(t);
-    double error = exp(t) - 1;
-    double last = 1;
-    double k = 0.;
+unsigned int get_taylor_degree(double t, double eps) {
+    auto eps_exp_t = eps * exp(t);
+    auto error = exp(t) - 1;
+    auto last = 1.0;
+    auto k = 0u;
     while (error > eps_exp_t) {
-        k = k + 1;
-        last = (last * t) / k;
-        error = error - last;
+        k++;
+        last *= t / k;
+        error -= last;
     }
-    return max(static_cast<unsigned int>(k), 1u);
+    return max(k, 1u);
 }
 
 
@@ -94,40 +90,41 @@ unsigned int get_taylor_degree(const double t, const double eps) {
  *      Q - the queue data structure
  */
 template<class Queue>
-int gsqexpmseed(sparserow *G, sparsevec &set, sparsevec &y, const double t, const double eps,
-                const mwIndex max_push_count, Queue &Q) {
-    mwIndex n = G->n;
-    mwIndex N = (mwIndex) get_taylor_degree(t, eps);
+mwIndex gsqexpmseed(sparse_row *graph, sparse_vec &set, sparse_vec &y, const double t, const double eps,
+                    const mwIndex max_push_count, Queue &Q) {
+    mwIndex n = graph->n_;
+    mwIndex taylor_deg = (mwIndex) get_taylor_degree(t, eps);
 
     // initialize the weights for the different residual partitions
-    // r(i,j) > d(i)*exp(t)*eps/(N*psi_j(t))
+    // r(i,j) > d(i)*exp(t)*eps/(taylor_deg*psi_j(t))
     //  since each coefficient but d(i) stays the same,
     //  we combine all coefficients except d(i)
-    //  into the vector "pushcoeff"
-    vector<double> psivec(N + 1, 0.);
-    psivec[N] = 1;
-    for (int k = 1; k <= N; k++) {
-        psivec[N - k] = psivec[N - k + 1] * t / (double) (N - k + 1) + 1;
-    } // psivec[k] = psi_k(t)
-    vector<double> pushcoeff(N + 1, 0.);
-    pushcoeff[0] = ((exp(t) * eps) / (double) N) / psivec[0]; // This is the correct version
-    for (int k = 1; k <= N; k++) {
-        pushcoeff[k] = pushcoeff[k - 1] * (psivec[k - 1] / psivec[k]);
-    } // pushcoeff[j] = exp(t)*eps/(N*psivec[j])
+    //  into the vector "push_coeff"
+    // psi_vec[k] = psi_k(t)
+
+    vector<double> psi_vec(taylor_deg + 1, 0);
+    psi_vec[taylor_deg] = 1;
+    for (int k = 1; k <= taylor_deg; k++) {
+        psi_vec[taylor_deg - k] = psi_vec[taylor_deg - k + 1] * t / (double) (taylor_deg - k + 1) + 1;
+    }
+
+    vector<double> push_coeff(taylor_deg + 1, 0.);
+    push_coeff[0] = ((exp(t) * eps) / (double) taylor_deg) / psi_vec[0]; // This is the correct version
+    for (int k = 1; k <= taylor_deg; k++) {
+        push_coeff[k] = push_coeff[k - 1] * (psi_vec[k - 1] / psi_vec[k]);
+    } // push_coeff[j] = exp(t)*eps/(taylor_deg*psi_vec[j])
 
     mwIndex ri = 0;
     mwIndex npush = 0;
     double rij = 0;
-    // allocate data
-    sparsevec rvec;
 
+    sparse_vec rvec;
     // i is the node index, j is the "step"
-
     // set the initial residual, add to the queue
-    for (auto it = set.map.begin(), itend = set.map.end(); it != itend; ++it) {
+    for (auto it = set.weight_map_.begin(), itend = set.weight_map_.end(); it != itend; ++it) {
         ri = it->first;
         rij = it->second;
-        rvec.map[rentry(ri, 0)] += rij;
+        rvec.weight_map_[rentry(ri, 0)] += rij;
         Q.push(rentry(ri, 0));
     }
 
@@ -138,115 +135,107 @@ int gsqexpmseed(sparserow *G, sparsevec &set, sparsevec &y, const double t, cons
         mwIndex i = ri % n;
         mwIndex j = ri / n;
 
-        double degofi = (double) G->sr_degree(i);
-        rij = rvec.map[ri];
+        double degofi = (double) graph->sr_degree(i);
+        rij = rvec.weight_map_[ri];
 
-        y.map[i] += rij;
+        y.weight_map_[i] += rij;
 
         // update r, no need to update heap here
-        rvec.map[ri] = 0;
+        rvec.weight_map_[ri] = 0;
 
         double rijs = t * rij / (double) (j + 1);
         double ajv = 1. / degofi;
         double update = rijs * ajv;
 
-        if (j == N - 1) {
+        if (j == taylor_deg - 1) {
             // this is the terminal case, and so we add the column of A, directly to the solution vector y
-            for (mwIndex nzi = G->ai[i]; nzi < G->ai[i + 1]; ++nzi) {
-                mwIndex v = G->aj[nzi];
-                y.map[v] += update;
+            for (mwIndex nzi = graph->vertices_[i]; nzi < graph->vertices_[i + 1]; ++nzi) {
+                mwIndex v = graph->edges_[nzi];
+                y.weight_map_[v] += update;
             }
             npush += degofi;
         } else {
             // this is the interior case, and so we add the column of A
             // to the residual at the next time step.
-            for (mwIndex nzi = G->ai[i]; nzi < G->ai[i + 1]; ++nzi) {
-                mwIndex v = G->aj[nzi];
+            for (mwIndex nzi = graph->vertices_[i]; nzi < graph->vertices_[i + 1]; ++nzi) {
+                mwIndex v = graph->edges_[nzi];
                 mwIndex re = rentry(v, j + 1);
                 double reold = rvec.get(re);
                 double renew = reold + update;
-                double dv = G->sr_degree(v);
-                rvec.map[re] = renew;
-                if (renew >= dv * pushcoeff[j + 1] && reold < dv * pushcoeff[j + 1]) {
+                double dv = graph->sr_degree(v);
+                rvec.weight_map_[re] = renew;
+                if (renew >= dv * push_coeff[j + 1] && reold < dv * push_coeff[j + 1]) {
                     Q.push(re);
                 }
             }
             npush += degofi;
         }
-        // terminate when Q is empty, i.e. we've pushed all r(i,j) > eps*exp(t)*d(i)/(N*psi_j(t))
+        // terminate when Q is empty, i.e. we've pushed all r(i,j) > eps*exp(t)*d(i)/(taylor_deg*psi_j(t))
         if (Q.size() == 0) { return npush; }
     }//end 'while'
     return npush;
 }
 
 
-void cluster_from_sweep(sparserow *G, sparsevec &p, vector<mwIndex> &cluster, double *outcond, double *outvolume,
+void cluster_from_sweep(sparse_row *G, sparse_vec &p, vector<mwIndex> &cluster, double *outcond, double *outvolume,
                         double *outcut) {
     // now we have to do the sweep over p in sorted order by value
-    typedef vector<pair<int, double> > vertex_prob_type;
-    vertex_prob_type prpairs(p.map.begin(), p.map.end());
-
-    sort(prpairs.begin(), prpairs.end(), [](auto &&left, auto &&right) { return left.second > right.second; });
+    vector<pair<size_t, double>> pr_pairs(p.weight_map_.begin(), p.weight_map_.end());
+    sort(pr_pairs.begin(), pr_pairs.end(), [](auto &&left, auto &&right) { return left.second > right.second; });
 
     // compute cutsize, volume, and conductance
-    vector<double> conductance(prpairs.size());
-    vector<mwIndex> volume(prpairs.size());
-    vector<mwIndex> cutsize(prpairs.size());
+    vector<double> conductance(pr_pairs.size());
+    vector<mwIndex> volume(pr_pairs.size());
+    vector<mwIndex> cutsize(pr_pairs.size());
 
-    size_t i = 0;
-    unordered_map<int, size_t> rank;
-    for (auto it = prpairs.begin(), itend = prpairs.end();
-         it != itend; ++it, ++i) {
-        rank[it->first] = i;
+    unordered_map<size_t, size_t> rank_map;
+
+    for (auto i = 0ul; i < pr_pairs.size(); i++) {
+        rank_map[pr_pairs[i].first] = i;
     }
-    mwIndex total_degree = G->ai[G->m];
+    mwIndex total_degree = G->vertices_[G->m_];
     mwIndex curcutsize = 0;
     mwIndex curvolume = 0;
-    i = 0;
-    for (auto it = prpairs.begin(), itend = prpairs.end(); it != itend; ++it, ++i) {
-        mwIndex v = it->first;
-        mwIndex deg = G->ai[v + 1] - G->ai[v];
+
+    for (auto i = 0ul; i < pr_pairs.size(); i++) {
+        mwIndex v = pr_pairs[i].first;
+        mwIndex deg = G->vertices_[v + 1] - G->vertices_[v];
         mwIndex change = deg;
-        for (mwIndex nzi = G->ai[v]; nzi < G->ai[v + 1]; ++nzi) {
-            mwIndex nbr = G->aj[nzi];
-            if (rank.count(nbr) > 0) {
-                if (rank[nbr] < rank[v]) {
-                    change -= 2;
-                }
+        for (mwIndex nzi = G->vertices_[v]; nzi < G->vertices_[v + 1]; ++nzi) {
+            mwIndex nbr = G->edges_[nzi];
+            if (rank_map.count(nbr) > 0 && rank_map[nbr] < rank_map[v]) {
+                change -= 2;
             }
         }
+
         curcutsize += change;
-        //if (curvolume + deg > target_vol) {
-        //break;
-        //}
         curvolume += deg;
         volume[i] = curvolume;
         cutsize[i] = curcutsize;
         if (curvolume == 0 || total_degree - curvolume == 0) {
             conductance[i] = 1;
         } else {
-            conductance[i] = (double) curcutsize /
-                             (double) min(curvolume, total_degree - curvolume);
+            conductance[i] = static_cast<double>(curcutsize) / min(curvolume, total_degree - curvolume);
         }
     }
 
     // we stopped the iteration when it finished, or when it hit target_vol
-    size_t lastind = i;
-    double mincond = numeric_limits<double>::max();
-    size_t mincondind = 0; // set to zero so that we only add one vertex
-    for (i = 0; i < lastind; i++) {
-        if (conductance[i] < mincond) {
-            mincond = conductance[i];
-            mincondind = i;
+    double min_cond = numeric_limits<double>::max();
+    size_t min_cond_idx = 0; // set to zero so that we only add one vertex
+
+    for (auto i = 0ul; i < pr_pairs.size(); i++) {
+        if (conductance[i] < min_cond) {
+            min_cond = conductance[i];
+            min_cond_idx = i;
         }
     }
-    if (lastind == 0) {
-        mincond = 0.0;
+    if (pr_pairs.size() == 0) {
+        min_cond = 0.0;
     }
-    for (auto &ele:prpairs) { cluster.emplace_back(ele.first); }
-    if (outcond) { *outcond = mincond; }
-    if (outvolume) { *outvolume = volume[mincondind]; }
-    if (outcut) { *outcut = cutsize[mincondind]; }
+    for (auto &ele:pr_pairs) { cluster.emplace_back(ele.first); }
+    if (outcond) { *outcond = min_cond; }
+    if (outvolume) { *outvolume = volume[min_cond_idx]; }
+    if (outcut) { *outcut = cutsize[min_cond_idx]; }
 }
 
 struct local_hkpr_stats {
@@ -259,54 +248,49 @@ struct local_hkpr_stats {
 };
 
 /** Cluster will contain a list of all the vertices in the cluster
- * @param set the set of starting vertices to use
- * @param t the value of t in the heatkernelPageRank computation
+ * @param seed_set the set of starting vertices to use
+ * @param t the value of t in the heat-kernel-PageRank computation
  * @param eps the solution tolerance eps
- * @param p the heatkernelpagerank vector
+ * @param p the heat-kernel-pagerank vector
  * @param r the residual vector
  * @param a vector which supports .push_back to add vertices for the cluster
  * @param stats a structure for statistics of the computation
  */
 template<class Queue>
-int hypercluster_heatkernel_multiple(sparserow *G, const vector<mwIndex> &set, double t, double eps,
-                                     sparsevec &p, sparsevec &r, Queue &q,
-                                     vector<mwIndex> &cluster, local_hkpr_stats *stats) {
-    // reset data
-    p.map.clear();
-    r.map.clear();
+int hyper_cluster_heat_kernel_multiple(sparse_row *G, const vector<mwIndex> &seed_set, double t, double eps,
+                                       sparse_vec &p, sparse_vec &r, Queue &q, vector<mwIndex> &cluster,
+                                       local_hkpr_stats *stats) {
+    p.weight_map_.clear();
+    r.weight_map_.clear();
     q.empty();
 
-    size_t maxdeg = 0;
-    for (size_t i = 0; i < set.size(); ++i) { //populate r with indices of "set"
-        assert(set[i] >= 0);
-        assert(set[i] < G->n); // assert that "set" contains indices i: 1<=i<=n
-        size_t setideg = G->sr_degree(set[i]);
-        r.map[set[i]] = 1. / (double) (set.size()); // r is normalized to be stochastic
-        maxdeg = max(maxdeg, setideg);
+    size_t max_deg = 0;
+    for (size_t i = 0; i < seed_set.size(); ++i) {
+        size_t v_degree = G->sr_degree(seed_set[i]);
+        r.weight_map_[seed_set[i]] = 1.0 / seed_set.size();
+        max_deg = max(max_deg, v_degree);
     }
 
-    int nsteps = gsqexpmseed(G, r, p, t, eps, static_cast<int>(ceil(pow(G->n, 1.5))), q);
+    auto nsteps = gsqexpmseed(G, r, p, t, eps, static_cast<size_t >(ceil(pow(G->n_, 1.5))), q);
 
     if (nsteps == 0) {
         p = r; // just copy over the residual
     }
-    int support = static_cast<int>(r.map.size());
+
     if (stats) { stats->steps = nsteps; }
-    if (stats) { stats->support = support; }
+    if (stats) { stats->support = static_cast<int>(r.weight_map_.size()); }
 
     // scale the probablities by their degree
-    for (auto it = p.map.begin(), itend = p.map.end();
-         it != itend; ++it) {
-        it->second *= (1.0 / (double) max(G->sr_degree(it->first), (mwIndex) 1));
-    }
+    for (auto &ele:p.weight_map_) { ele.second *= (1.0 / max(G->sr_degree(ele.first), (mwIndex) 1)); }
 
-    double *outcond = NULL;
-    double *outvolume = NULL;
-    double *outcut = NULL;
-    if (stats) { outcond = &stats->conductance; }
-    if (stats) { outvolume = &stats->volume; }
-    if (stats) { outcut = &stats->cut; }
-    cluster_from_sweep(G, p, cluster, outcond, outvolume, outcut);
+    double *out_cond = nullptr;
+    double *out_volume = nullptr;
+    double *out_cut = nullptr;
+    if (stats) { out_cond = &stats->conductance; }
+    if (stats) { out_volume = &stats->volume; }
+    if (stats) { out_cut = &stats->cut; }
+
+    cluster_from_sweep(G, p, cluster, out_cond, out_volume, out_cut);
     return 0;
 }
 
@@ -320,14 +304,13 @@ int hypercluster_heatkernel_multiple(sparserow *G, const vector<mwIndex> &set, d
  * @param fcut the final cut score of the set
  * @param fvol the final volume score of the set
  */
-void hkgrow(sparserow *G, vector<mwIndex> &seeds, double t,
-            double eps, double *fcond, double *fcut,
-            double *fvol, sparsevec &p, double *npushes) {
-    sparsevec r;
+void hk_grow(sparse_row *G, vector<mwIndex> &seeds, double t, double eps, double *fcond, double *fcut,
+             double *fvol, sparse_vec &p, double *npushes) {
+    sparse_vec r;
     queue<mwIndex> q;
     local_hkpr_stats stats;
     vector<mwIndex> bestclus;
-    hypercluster_heatkernel_multiple(G, seeds, t, eps, p, r, q, bestclus, &stats);
+    hyper_cluster_heat_kernel_multiple(G, seeds, t, eps, p, r, q, bestclus, &stats);
     seeds = bestclus;
     *npushes = stats.steps;
     *fcond = stats.conductance;
@@ -337,12 +320,12 @@ void hkgrow(sparserow *G, vector<mwIndex> &seeds, double t,
 
 /*
 void copy_array_to_index_vector(const mxArray *v, vector<mwIndex> &vec) {
-    size_t n = mxGetNumberOfElements(v);
+    size_t n_ = mxGetNumberOfElements(v);
     double *p = mxGetPr(v);
 
-    vec.resize(n);
+    vec.resize(n_);
 
-    for (size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < n_; ++i) {
         double elem = p[i];
         mxAssert(elem >= 1, "Only positive integer elements allowed");
         vec[i] = (mwIndex) elem - 1;
@@ -377,18 +360,18 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         eps = mxGetScalar(prhs[3]);
     }
 
-    sparserow r;
-    r.m = mxGetM(mat);
-    r.n = mxGetN(mat);
-    r.ai = mxGetJc(mat);
-    r.aj = mxGetIr(mat);
-    r.a = mxGetPr(mat);
+    sparse_row r;
+    r.m_ = mxGetM(mat);
+    r.n_ = mxGetN(mat);
+    r.vertices_ = mxGetJc(mat);
+    r.edges_ = mxGetIr(mat);
+    r.weight_ = mxGetPr(mat);
 
     vector<mwIndex> seeds;
     copy_array_to_index_vector(set, seeds);
-    sparsevec hkpr;
+    sparse_vec hkpr;
 
-    hkgrow(&r, seeds, t, eps, mxGetPr(cond), mxGetPr(cut), mxGetPr(vol), hkpr, mxGetPr(npushes));
+    hk_grow(&r, seeds, t, eps, mxGetPr(cond), mxGetPr(cut), mxGetPr(vol), hkpr, mxGetPr(npushes));
 
     if (nlhs > 0) { // sets output "bestset" to the set of best conductance
         mxArray *cassign = mxCreateDoubleMatrix(seeds.size(), 1, mxREAL);
@@ -400,10 +383,10 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
         }
     }
     if (nlhs > 4) { // sets output "y" to the heat kernel vector computed
-        mxArray *hkvec = mxCreateDoubleMatrix(r.n, 1, mxREAL);
+        mxArray *hkvec = mxCreateDoubleMatrix(r.n_, 1, mxREAL);
         plhs[4] = hkvec;
         double *ci = mxGetPr(hkvec);
-        for (sparsevec:: auto it = hkpr.map.begin(), itend = hkpr.map.end();
+        for (sparse_vec:: auto it = hkpr.weight_map_.begin(), itend = hkpr.weight_map_.end();
              it != itend; ++it) {
             ci[it->first] = it->second;
         }
