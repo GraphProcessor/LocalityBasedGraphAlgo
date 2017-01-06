15ff48beaefebf13d75a3dd93772353f3d9852cc
cheyulin <yche@cse.ust.hk>
2017-01-06 17:30:13 +0800

    refactor some

--- src/algorithm/ReadMe.md
+++ src/algorithm/ReadMe.md
@@ -6,3 +6,73 @@
 ##Sequential Implementation
 - CIs: [cis_sequential_algorithm.h](sequential/cis_sequential_algorithm.h), [cis_sequential_algorithm.cpp](sequential/cis_sequential_algorithm.cpp)
 - Demon: [demon_sequential_algorithm.h](sequential/demon_sequential_algorithm.h), [demon_seqential_algorithm.cpp](sequential/demon_seqential_algorithm.cpp)
+
+##HrGrow Algo
+
+- parameters
+
+```zsh
+
+/*
+// [bestset,cond,cut,vol,y,npushes] = hkgrow_mex(A,set,t,eps,debugflag)
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    if (nrhs == 5) {
+        debugflag = (int) mxGetScalar(prhs[4]);
+    }
+
+    const mxArray *mat = prhs[0];
+    const mxArray *set = prhs[1];
+
+    mxArray *cond = mxCreateDoubleMatrix(1, 1, mxREAL);
+    mxArray *cut = mxCreateDoubleMatrix(1, 1, mxREAL);
+    mxArray *vol = mxCreateDoubleMatrix(1, 1, mxREAL);
+    mxArray *npushes = mxCreateDoubleMatrix(1, 1, mxREAL);
+
+    if (nlhs > 1) { plhs[1] = cond; }
+    if (nlhs > 2) { plhs[2] = cut; }
+    if (nlhs > 3) { plhs[3] = vol; }
+    if (nlhs > 5) { plhs[5] = npushes; }
+
+    double eps = pow(10, -3);
+    double t = 15.;
+
+    if (nrhs >= 4) {
+        t = mxGetScalar(prhs[2]);
+        eps = mxGetScalar(prhs[3]);
+    }
+
+    sparse_row r;
+    r.m_ = mxGetM(mat);
+    r.n_ = mxGetN(mat);
+    r.vertices_ = mxGetJc(mat);
+    r.edges_ = mxGetIr(mat);
+    r.weight_ = mxGetPr(mat);
+
+    vector<mwIndex> seeds;
+    copy_array_to_index_vector(set, seeds);
+    sparse_vec hkpr;
+
+    hk_grow(&r, seeds, t, eps, mxGetPr(cond), mxGetPr(cut), mxGetPr(vol), hkpr, mxGetPr(npushes));
+
+    if (nlhs > 0) { // sets output "bestset" to the set of best conductance
+        mxArray *cassign = mxCreateDoubleMatrix(seeds.size(), 1, mxREAL);
+        plhs[0] = cassign;
+
+        double *ci = mxGetPr(cassign);
+        for (size_t i = 0; i < seeds.size(); ++i) {
+            ci[i] = (double) (seeds[i] + 1);
+        }
+    }
+    if (nlhs > 4) { // sets output "y" to the heat kernel vector computed
+        mxArray *hkvec = mxCreateDoubleMatrix(r.n_, 1, mxREAL);
+        plhs[4] = hkvec;
+        double *ci = mxGetPr(hkvec);
+        for (sparse_vec:: auto it = hkpr.weight_map_.begin(), itend = hkpr.weight_map_.end();
+             it != itend; ++it) {
+            ci[it->first] = it->second;
+        }
+    }
+}*/
+
+```
+
--- src/algorithm/sequential/hr_grow_sequential_algorithm.h
+++ src/algorithm/sequential/hr_grow_sequential_algorithm.h
@@ -8,7 +8,6 @@
 /**
  * Implement a seeded heat-kernel clustering scheme.
  * [bestset,cond,cut,vol,y,npushes] = hkgrow_mex(A,set,t,eps,debugflag)
- * mex -O CXXFLAGS="\$CXXFLAGS -std=c++0x" -largeArrayDims hkgrow_mex.cpp
  */
 
 #include <cmath>
@@ -89,9 +88,8 @@ unsigned int get_taylor_degree(double t, double eps) {
  *      max_push_count - the total number of steps to run
  *      Q - the queue data structure
  */
-template<class Queue>
-mwIndex gsqexpmseed(sparse_row *graph, sparse_vec &set, sparse_vec &y, const double t, const double eps,
-                    const mwIndex max_push_count, Queue &Q) {
+mwIndex gs_qexpm_seed(sparse_row *graph, sparse_vec &set, sparse_vec &y, const double t, const double eps,
+                      const mwIndex max_push_count, queue<mwIndex> &Q) {
     mwIndex n = graph->n_;
     mwIndex taylor_deg = (mwIndex) get_taylor_degree(t, eps);
 
@@ -256,9 +254,8 @@ struct local_hkpr_stats {
  * @param a vector which supports .push_back to add vertices for the cluster
  * @param stats a structure for statistics of the computation
  */
-template<class Queue>
 int hyper_cluster_heat_kernel_multiple(sparse_row *G, const vector<mwIndex> &seed_set, double t, double eps,
-                                       sparse_vec &p, sparse_vec &r, Queue &q, vector<mwIndex> &cluster,
+                                       sparse_vec &p, sparse_vec &r, queue<mwIndex> &q, vector<mwIndex> &cluster,
                                        local_hkpr_stats *stats) {
     p.weight_map_.clear();
     r.weight_map_.clear();
@@ -271,7 +268,7 @@ int hyper_cluster_heat_kernel_multiple(sparse_row *G, const vector<mwIndex> &see
         max_deg = max(max_deg, v_degree);
     }
 
-    auto nsteps = gsqexpmseed(G, r, p, t, eps, static_cast<size_t >(ceil(pow(G->n_, 1.5))), q);
+    auto nsteps = gs_qexpm_seed(G, r, p, t, eps, static_cast<size_t >(ceil(pow(G->n_, 1.5))), q);
 
     if (nsteps == 0) {
         p = r; // just copy over the residual
@@ -280,7 +277,7 @@ int hyper_cluster_heat_kernel_multiple(sparse_row *G, const vector<mwIndex> &see
     if (stats) { stats->steps = nsteps; }
     if (stats) { stats->support = static_cast<int>(r.weight_map_.size()); }
 
-    // scale the probablities by their degree
+    // scale the probabilities by their degree
     for (auto &ele:p.weight_map_) { ele.second *= (1.0 / max(G->sr_degree(ele.first), (mwIndex) 1)); }
 
     double *out_cond = nullptr;
@@ -317,80 +314,5 @@ void hk_grow(sparse_row *G, vector<mwIndex> &seeds, double t, double eps, double
     *fcut = stats.cut;
     *fvol = stats.volume;
 }
-
-/*
-void copy_array_to_index_vector(const mxArray *v, vector<mwIndex> &vec) {
-    size_t n_ = mxGetNumberOfElements(v);
-    double *p = mxGetPr(v);
-
-    vec.resize(n_);
-
-    for (size_t i = 0; i < n_; ++i) {
-        double elem = p[i];
-        mxAssert(elem >= 1, "Only positive integer elements allowed");
-        vec[i] = (mwIndex) elem - 1;
-    }
-}
-
-
-// [bestset,cond,cut,vol,y,npushes] = hkgrow_mex(A,set,t,eps,debugflag)
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-    if (nrhs == 5) {
-        debugflag = (int) mxGetScalar(prhs[4]);
-    }
-
-    const mxArray *mat = prhs[0];
-    const mxArray *set = prhs[1];
-
-    mxArray *cond = mxCreateDoubleMatrix(1, 1, mxREAL);
-    mxArray *cut = mxCreateDoubleMatrix(1, 1, mxREAL);
-    mxArray *vol = mxCreateDoubleMatrix(1, 1, mxREAL);
-    mxArray *npushes = mxCreateDoubleMatrix(1, 1, mxREAL);
-
-    if (nlhs > 1) { plhs[1] = cond; }
-    if (nlhs > 2) { plhs[2] = cut; }
-    if (nlhs > 3) { plhs[3] = vol; }
-    if (nlhs > 5) { plhs[5] = npushes; }
-
-    double eps = pow(10, -3);
-    double t = 15.;
-
-    if (nrhs >= 4) {
-        t = mxGetScalar(prhs[2]);
-        eps = mxGetScalar(prhs[3]);
-    }
-
-    sparse_row r;
-    r.m_ = mxGetM(mat);
-    r.n_ = mxGetN(mat);
-    r.vertices_ = mxGetJc(mat);
-    r.edges_ = mxGetIr(mat);
-    r.weight_ = mxGetPr(mat);
-
-    vector<mwIndex> seeds;
-    copy_array_to_index_vector(set, seeds);
-    sparse_vec hkpr;
-
-    hk_grow(&r, seeds, t, eps, mxGetPr(cond), mxGetPr(cut), mxGetPr(vol), hkpr, mxGetPr(npushes));
-
-    if (nlhs > 0) { // sets output "bestset" to the set of best conductance
-        mxArray *cassign = mxCreateDoubleMatrix(seeds.size(), 1, mxREAL);
-        plhs[0] = cassign;
-
-        double *ci = mxGetPr(cassign);
-        for (size_t i = 0; i < seeds.size(); ++i) {
-            ci[i] = (double) (seeds[i] + 1);
-        }
-    }
-    if (nlhs > 4) { // sets output "y" to the heat kernel vector computed
-        mxArray *hkvec = mxCreateDoubleMatrix(r.n_, 1, mxREAL);
-        plhs[4] = hkvec;
-        double *ci = mxGetPr(hkvec);
-        for (sparse_vec:: auto it = hkpr.weight_map_.begin(), itend = hkpr.weight_map_.end();
-             it != itend; ++it) {
-            ci[it->first] = it->second;
-        }
-    }
-}*/
 
 #endif //CODES_YCHE_HR_GROW_SEQUENTIAL_ALGORITHM_H
