35f47fa8c0c97749d078f30f00001b999ee49580
cheyulin <yche@cse.ust.hk>
2017-02-13 09:25:47 +0800

    update python script to report time

--- scripts/analyze_algo_quality.py
+++ scripts/analyze_algo_quality.py
@@ -23,12 +23,13 @@ def get_community_result(file_path):
         lines = ifs.readlines()
         comm_size = int(get_val('comm size', lines))
         name_res_str = get_val('name result', lines)
+        whole_time_str = get_val('whole execution time', lines)
         comm_list = eval(name_res_str)
 
         for comm in comm_list:
             comm.sort()
         comm_list.sort(key=lambda comm: -len(comm))
-        return comm_size, comm_list
+        return comm_size, comm_list, whole_time_str
 
 
 def print_comm(comm_list):
@@ -39,7 +40,7 @@ def print_comm(comm_list):
 if __name__ == '__main__':
     graph = get_graph_info('../datasets/social_network/facebook_combined.txt')
     print 'num nodes:', graph.number_of_nodes(), 'num edges:', graph.number_of_edges()
-    comm_num, comm_list = get_community_result('../src/cmake-build-debug/algorithm_demo/demon_fb.out')
+    comm_num, comm_list, run_time = get_community_result('../src/cmake-build-debug/algorithm_demo/demon_fb.out')
     avg_comm_size = sum(map(lambda ele: len(ele), comm_list)) / comm_num
-    print 'comm num:', comm_num, 'avg comm size:', avg_comm_size
+    print 'comm num:', comm_num, 'avg comm size:', avg_comm_size, 'whole execution time:', run_time
     print cal_modularity(graph, comm_list)
